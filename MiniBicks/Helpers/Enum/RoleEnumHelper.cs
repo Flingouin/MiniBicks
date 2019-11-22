dd91dcae312bfaa40b46cc85a73f295743fcc8b1
Flingouin <matthieu.fritsch@viacesi.fr>
2019-11-22 15:10:08 +0100

    Maj CSS

---
+++ MiniBicks/Helpers/Enum/RoleEnumHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MiniBicks.Entities.Enum;
+
+namespace MiniBicks.Helpers.Enum
+{
+    public static class RoleEnumHelper
+    {
+        public static string GetFriendlyEnum(RoleEnum roleEnum)
+        {
+            string result = string.Empty;
+            switch (roleEnum)
+            {
+                case RoleEnum.Collaborateur:
+                    result = "Collaborateur";
+                    break;
+                case RoleEnum.Manager:
+                    result = "Manager";
+                    break;
+                case RoleEnum.ComiteDirection:
+                    result = "Comité de direction";
+                    break;
+                case RoleEnum.PDG:
+                    result = "PDG";
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
